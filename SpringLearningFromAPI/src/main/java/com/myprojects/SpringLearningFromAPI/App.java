d8efd76d228dcffd8a22e6a41fde04132089333d
arun <arun@arun-Ideapad-Z570>
2013-10-10 21:56:26 +1100

    initialsetup

---
+++ SpringLearningFromAPI/src/main/java/com/myprojects/SpringLearningFromAPI/App.java
@@ -0,0 +1,13 @@
+package com.myprojects.SpringLearningFromAPI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
