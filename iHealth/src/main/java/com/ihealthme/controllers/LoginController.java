d40d258e2b35ea96858205a6095b17e526f3e710
arun <arun@arun-Ideapad-Z570>
2014-03-01 12:58:47 +1100

    webxml updated

---
+++ iHealth/src/main/java/com/ihealthme/controllers/LoginController.java
@@ -0,0 +1,14 @@
+package com.ihealthme.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showLoginPage() {
+        return "user/login";
+    }
+}
